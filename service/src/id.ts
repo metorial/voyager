144b4045fc1bc0038cc58d78464d3eb9d534bce3
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-09 09:02:27 +0100

    Add data models

---
+++ service/src/id.ts
@@ -0,0 +1,31 @@
+import { createIdGenerator, idType } from '@lowerdeck/id';
+import { Worker as SnowflakeId } from 'snowflake-uuid';
+
+export let ID = createIdGenerator({
+  tenant: idType.sorted('vtn_'),
+  source: idType.sorted('vsn_'),
+  index: idType.sorted('vix_'),
+  backend: idType.sorted('vbe_'),
+  record: idType.sorted('vrc_')
+});
+
+let workerIdBits = 12;
+let workerIdMask = (1 << workerIdBits) - 1;
+
+let workerId = (() => {
+  let array = new Uint16Array(1);
+  crypto.getRandomValues(array);
+  return array[0]! & workerIdMask;
+})();
+
+export let snowflake = new SnowflakeId(workerId, 0, {
+  workerIdBits: workerIdBits,
+  datacenterIdBits: 0,
+  sequenceBits: 9,
+  epoch: new Date('2025-06-01T00:00:00Z').getTime()
+});
+
+export let getId = <K extends Parameters<typeof ID.generateIdSync>[0]>(model: K) => ({
+  oid: snowflake.nextId(),
+  id: ID.generateIdSync(model)
+});
