516b87d4bbd9eb9c4b7362fdf375f500016cb5b2
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-09 18:39:37 +0100

    add custom search db params

--- service/src/init.ts
+++ service/src/init.ts
@@ -2,6 +2,10 @@ if (!process.env.DATABASE_URL) {
   process.env.DATABASE_URL = `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?schema=public&sslmode=no-verify&connection_limit=20`;
 }
 
+if (!process.env.SEARCH_DATABASE_URL_URL) {
+  process.env.SEARCH_DATABASE_URL_URL = `postgres://${process.env.SEARCH_DATABASE_URL_USERNAME}:${process.env.SEARCH_DATABASE_URL_PASSWORD}@${process.env.SEARCH_DATABASE_URL_HOST}:${process.env.SEARCH_DATABASE_URL_PORT}/${process.env.SEARCH_DATABASE_URL_NAME}`;
+}
+
 if (!process.env.REDIS_URL) {
   process.env.REDIS_URL = `${process.env.REDIS_TLS == 'true' ? 'rediss' : 'redis'}://:${process.env.REDIS_AUTH_TOKEN}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`;
 } else {
