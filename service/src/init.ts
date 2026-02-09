472c35c1896aab2560a8e6d9ef63f53e8316bffc
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-09 19:07:37 +0100

    oops

--- service/src/init.ts
+++ service/src/init.ts
@@ -2,8 +2,8 @@ if (!process.env.DATABASE_URL) {
   process.env.DATABASE_URL = `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?schema=public&sslmode=no-verify&connection_limit=20`;
 }
 
-if (!process.env.SEARCH_DATABASE_URL_URL) {
-  process.env.SEARCH_DATABASE_URL_URL = `postgres://${process.env.SEARCH_DATABASE_URL_USERNAME}:${process.env.SEARCH_DATABASE_URL_PASSWORD}@${process.env.SEARCH_DATABASE_URL_HOST}:${process.env.SEARCH_DATABASE_URL_PORT}/${process.env.SEARCH_DATABASE_URL_NAME}`;
+if (!process.env.SEARCH_DATABASE_URL) {
+  process.env.SEARCH_DATABASE_URL = `postgres://${process.env.SEARCH_DATABASE_URL_USERNAME}:${process.env.SEARCH_DATABASE_URL_PASSWORD}@${process.env.SEARCH_DATABASE_URL_HOST}:${process.env.SEARCH_DATABASE_URL_PORT}/${process.env.SEARCH_DATABASE_URL_NAME}`;
 }
 
 if (!process.env.REDIS_URL) {
