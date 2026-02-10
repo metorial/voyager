e133732373b1bff9bf99a2e81109f75e72119c41
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-10 09:44:19 +0100

    wip

--- service/prisma.config.ts
+++ service/prisma.config.ts
@@ -3,15 +3,6 @@ import { defineConfig } from 'prisma/config';
 
 let databaseUrl = process.env.DATABASE_URL;
 
-console.log({
-  databaseUrl,
-  DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-  DATABASE_HOST: process.env.DATABASE_HOST,
-  DATABASE_PORT: process.env.DATABASE_PORT,
-  DATABASE_NAME: process.env.DATABASE_NAME
-});
-
 if (
   !databaseUrl &&
   process.env.DATABASE_USERNAME &&
