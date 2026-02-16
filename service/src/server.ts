2e3f04435ba4fd2c99f935f47ffb1ff8239e7769
Ivan <vnxdeveloper@gmail.com>
2026-02-16 18:23:46 +0100

    Feat: Unify Docker Images

--- service/src/server.ts
+++ service/src/server.ts
@@ -1,4 +1,18 @@
-await import('./init');
-await import('./instrument');
-await import('./endpoints');
-await import('./worker');
+import { createRequire } from 'module';
+
+// Provide CommonJS `require` in ESM runtime for bundled deps.
+const require = createRequire(import.meta.url);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).require = require;
+
+async function main() {
+  await import('./init');
+  await import('./instrument');
+  await import('./endpoints');
+  await import('./worker');
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
