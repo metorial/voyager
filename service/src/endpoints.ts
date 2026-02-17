9b5128c1eedf31956feb72f70933fe765883fa1e
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-17 17:16:33 +0100

    Fix redis pings

--- service/src/endpoints.ts
+++ service/src/endpoints.ts
@@ -1,4 +1,4 @@
-import { redis } from 'bun';
+import { RedisClient } from 'bun';
 import { VoyagerApi } from './controllers';
 import { db } from './db';
 
@@ -13,7 +13,12 @@ Bun.serve({
   fetch: async _ => {
     try {
       await db.backend.count();
+
+      let redis = new RedisClient(process.env.REDIS_URL?.replace('rediss://', 'redis://'), {
+        tls: process.env.REDIS_URL?.startsWith('rediss://')
+      });
       await redis.ping();
+
       return new Response('OK');
     } catch (e) {
       return new Response('Service Unavailable', { status: 503 });
